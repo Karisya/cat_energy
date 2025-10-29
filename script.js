f2fdbc74def3cf914c268f509a44ca38c0cd2e45
Karisya <karina-romanova-05@bk.ru>
2025-10-29 17:49:52 +0300

    creating componet map amd footer, creating styles for component

--- script.js
+++ script.js
@@ -1,25 +1,53 @@
-const slider = document.querySelector('.slider');
-const range = document.querySelector('.slider__range-js');
+document.addEventListener("DOMContentLoaded", () => {
+  const slider = document.querySelector(".slider");
+  const range = document.querySelector(".slider__range-js");
 
-range.addEventListener('input', () => {
-  slider.style.setProperty('--value', range.value + '%');
+  if (!slider || !range) return;
+
+  range.addEventListener("input", () => {
+    slider.style.setProperty("--value", range.value + "%");
+  });
 });
+
 
 ymaps.ready(init);
-  function init(){
-    var map = new ymaps.Map("map", {
-      center: [59.938631, 30.323037],
-      zoom: 18,
-      controls: ['zoomControl']
-    });
-var placemark = new ymaps.Placemark([59.938631, 30.323037], {
-    hintContent: 'Главный штаб',
-    balloonContent: 'Главный штаб'
-}, {
-    iconLayout: 'default#image', 
-    iconImageHref: "./img/map-icon.svg", 
-    iconImageSize: [113, 106],       
-    iconImageOffset: [-50, -70]  
-});
-map.geoObjects.add(placemark);
+
+function init() {
+  let centerCoords = [59.938631, 30.323037 - 0.006];
+  let zoomLevel = 16;
+
+  if (window.innerWidth < 768) {
+    centerCoords = [59.938631, 30.323037];
+    zoomLevel = 15;
   }
+  if (window.innerWidth < 400) {
+    centerCoords = [59.938631, 30.323037];
+  }
+
+  const iconSize = window.innerWidth < 400 ? [57, 53] : [113, 106];
+   const iconOffset = window.innerWidth < 400 ? [-28, -40] : [-50, -70];
+
+
+  const map = new ymaps.Map("map", {
+    center: centerCoords,
+    zoom: zoomLevel,
+    controls: ["zoomControl"],
+  });
+
+  const placemark = new ymaps.Placemark(
+    [59.938631, 30.323037],
+    {
+      hintContent: "Главный штаб",
+      balloonContent: "Главный штаб",
+    },
+    {
+      iconLayout: "default#image",
+      iconImageHref: "./img/map-icon.svg",
+      iconImageSize: iconSize,
+      iconImageOffset: iconOffset,
+    }
+  );
+
+  map.geoObjects.add(placemark);
+
+}
